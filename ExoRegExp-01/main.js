f19886ea7f7db0e16b6809253e6b86e1e5668313
Samuel Legrain <samuel.legrain@bstorm.be>
2022-11-29 14:11:40 +0100

    Add Exo RegExp 01

---
+++ ExoRegExp-01/main.js
@@ -0,0 +1,12 @@
+const urls_html = document.querySelector('#urls');
+const valid_urls_html = document.querySelector('#valid_urls');
+const urls ="http://www.monsite.codePays\nhttp://www.google.be\nhttp://www.yahoo.com\nhttp://www.amazon.nl\nhttp://www..nl\n123http://www.amazon.nl";
+console.log(urls);
+urls_html.innerText = urls;
+
+let regex = /^http:\/\/www\.[a-z]+\.[a-z]{2,3}$/gmi;
+
+let valid_urls = urls.match(regex);
+console.log(valid_urls);
+
+valid_urls.forEach((url)=>valid_urls_html.innerText += `${url}\n`);
